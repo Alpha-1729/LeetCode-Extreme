5065ee56fa58bcabb2cae26673ea8c8f55ec6d74
Alpha-1729 <10110011100011110000@tutanota.com>
2024-06-16 17:21:43 +0530

    LeetCode 1045

---
+++ 000-Solutions/1045-Customers_Who_Bought_All_Products/Solution.sql
@@ -0,0 +1,14 @@
+--  Customers Who Bought All Products
+/*
+ >>>>
+ >>>>
+ >>>>
+ >>>>
+ */
+
+
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id 
+HAVING 
+    COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product);
