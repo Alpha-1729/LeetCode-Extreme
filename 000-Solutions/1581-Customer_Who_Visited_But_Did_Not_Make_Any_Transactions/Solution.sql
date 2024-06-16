93740a2de34cbd9e267bccbe1f6b3c1fbd93cd3a
Alpha-1729 <10110011100011110000@tutanota.com>
2024-06-16 17:28:45 +0530

    LeetCode 1581

---
+++ 000-Solutions/1581-Customer_Who_Visited_But_Did_Not_Make_Any_Transactions/Solution.sql
@@ -0,0 +1,14 @@
+--  Customer Who Visited But Did Not Make Any Transactions
+/*
+ >>>>
+ >>>>
+ >>>>
+ >>>>
+ */
+
+SELECT customer_id, COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+    ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY customer_id;
